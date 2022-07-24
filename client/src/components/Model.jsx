6b7973c62f358deac6cf0ded7dfca15f20dcc979
average-joe25 <abhishekkrsingh369@gmail.com>
2022-07-24 19:54:30 +0530

    patch 1.2

--- client/src/components/Model.jsx
+++ client/src/components/Model.jsx
@@ -16,9 +16,9 @@ function Model(){
     return (<section className="model" > 
         <div className="container">
             <div className="row">
-                <div className="col-12 col-md-5 col-lg-5 forms"><Form id="userInputData" placeholder="Enter the Medical Absract HERE....." text={orgdata} edit={edit}/></div>
+                <div className="col-12 col-md-5 col-lg-5 forms"><Form id="userInputData" placeholder="Enter the Medical Absract HERE.....  " text={orgdata} edit={edit}/></div>
                 <div className="col-12 col-md-2 col-lg-2  forms"><Submit getdata={getdata}/></div>
-                <div className="col-12 col-md-5 col-lg-5 forms"><Form id="serverOutputData" text={newdata} edit={update}/></div>
+                <div className="col-12 col-md-5 col-lg-5 forms"><Form id="serverOutputData" placeholder="NOTE: Due to using free Heroku deployment of api, first call to api may take sometime, Since api goes to sleep after 20 mins of Inactivity."text={newdata} edit={update}/></div>
             </div>
         </div>
     </section>);
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const { response } = require('express');
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const request = require('request');
+const { markAsUntransferable } = require('worker_threads');
 
 const app = express();
 
@@ -12,40 +14,26 @@ app.use(express.urlencoded({extended: true,}));
 app.use(express.json());
 
 
-
 app.get('/', function (req, res) {
-  var clientServerOptions = {
-    uri: 'https://pubmed-api-test.herokuapp.com/',
-    body: JSON.stringify({'data': ""}),
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-  }
-  request(clientServerOptions, function (error, response) {
-    if(error){
-        console.log(error);
-    }
-  }); 
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.post('/data',function(req,res){
     const inputdata=req.body.data;
     var clientServerOptions = {
-          uri: 'https://pubmed-api-test.herokuapp.com/',
-          body: JSON.stringify({'data':inputdata}),
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json'
-          }
+      uri: 'https://pubmed-api-test.herokuapp.com/',
+      body: JSON.stringify({'data':inputdata}),
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json'
       }
-      request(clientServerOptions, function (error, response) {
-          if(error){
-            console.log(error);
-          }
-          res.send({'value':JSON.parse(response.body).value});
-      }); 
+    }
+    request(clientServerOptions, function (error, response) {
+        if(error){
+          console.log(error);
+        }
+        res.send({'value':JSON.parse(response.body).value});
+    }); 
 });
 
 app.listen(process.env.PORT||9000,function(){
